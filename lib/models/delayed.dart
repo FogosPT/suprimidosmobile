9e1e267d0e3ab48441b713d02846098a0beafdbb
Guilherme Cabral <eu@guilhermecabral.net>
2018-11-29 18:13:38 +0000

    delayed model

---
+++ lib/models/delayed.dart
@@ -0,0 +1,37 @@
+class Delayed {
+  String line;
+  String type;
+  String vendor;
+  String begin;
+  String end;
+  int min;
+  int max;
+  int timestamp;
+
+  Delayed({
+    this.line,
+    this.type,
+    this.vendor,
+    this.begin,
+    this.end,
+    this.min,
+    this.max,
+    this.timestamp,
+  });
+
+  String get direction => '$begin > $end';
+  int get time => this.timestamp;
+
+  factory Delayed.fromJson(Map<String, dynamic> map) {
+    return new Delayed(
+      line: map['line'],
+      type: map['type'],
+      vendor: map['vendor'],
+      begin: map['begin'],
+      end: map['end'],
+      min: map['min'],
+      max: map['max'],
+      timestamp: map['timestamp'],
+    );
+  }
+}
