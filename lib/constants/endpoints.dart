f6d1113c6abb50d029d05cf297d32587e1fda159
Guilherme Cabral <eu@guilhermecabral.net>
2018-12-05 11:15:46 +0000

    new endpoints

--- lib/constants/endpoints.dart
+++ lib/constants/endpoints.dart
@@ -1,5 +1,8 @@
 Map endpoints = {
   'supressedEndpoint': 'https://tomahock.com/cenas/suprimidos/data.php?line=',
+  'supressedWeekEndpoint':
+      'https://tomahock.com/cenas/suprimidos/data.php?lineWeek=',
   'delayedEndpoint':
       'https://tomahock.com/cenas/suprimidos/data.php?lineDelay=',
+  'getLocations': 'https://tomahock.com/cenas/suprimidos/getlines.php'
 };
