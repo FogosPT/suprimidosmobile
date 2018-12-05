ea4e9c2ed507f32d564309600145ecced507420b
Guilherme Cabral <eu@guilhermecabral.net>
2018-12-05 11:41:04 +0000

    new dynamic route

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:suprimidospt/pages/home.dart';
 import 'package:suprimidospt/pages/settings.dart';
 import 'package:suprimidospt/pages/delays.dart';
 import 'package:suprimidospt/pages/line.dart';
+import 'package:suprimidospt/pages/line_stats.dart';
 
 void main() => runApp(MyApp());
 
@@ -39,13 +40,23 @@ Route<dynamic> _getRoute(RouteSettings settings) {
   final List<String> path = settings.name.split('/');
   if (path[0] != '') return null;
   if (path[1] == 'line') {
-    return new MaterialPageRoute<void>(
-      settings: settings,
-      builder: (BuildContext context) => Line(
-            line: path[2],
-            type: path[3],
-          ),
-    );
+    if (path[3] == 'stats') {
+      return new MaterialPageRoute<void>(
+        settings: settings,
+        builder: (BuildContext context) => LineStats(
+              line: path[2],
+              type: path[3],
+            ),
+      );
+    } else {
+      return new MaterialPageRoute<void>(
+        settings: settings,
+        builder: (BuildContext context) => Line(
+              line: path[2],
+              type: path[3],
+            ),
+      );
+    }
   }
   return null;
 }
